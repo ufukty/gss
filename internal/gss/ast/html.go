20dd03a3d77d29c2f0aa4b0a9f4f1abca17edb64
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-09 18:34:58 +0300

    int/gss/ast: adds types for individual html tags

--- internal/gss/ast/html.go
+++ internal/gss/ast/html.go
@@ -2,15 +2,41 @@ package ast
 
 import "go.ufukty.com/gss/internal/gss/tokens"
 
-type Element struct {
+type Element interface {
+	elem()
+}
+
+type element struct {
 	Tag        tokens.Tag
 	Id         string
 	Classes    []string
-	Attributes map[string]string // <img src> etc.
-	Parent     *Element
-	Children   []*Element
+	Attributes map[string]string
+	Parent     Element
+	Children   []Element
 }
 
+type (
+	Div struct {
+		element
+		TextContent string
+	}
+
+	Span struct {
+		element
+		TextContent string
+	}
+
+	Img struct {
+		element
+		Src    string
+		SrcSet map[float64]string
+	}
+)
+
+func (Div) elem()  {}
+func (Span) elem() {}
+func (Img) elem()  {}
+
 type Html struct {
-	Root *Element
+	Root Element
 }
