48a796f0a9f64516a6ac78c07aa69c90be05c024
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-12 11:24:29 +0300

    int/gss/defaults: adds a dispatcher

--- internal/gss/defaults/defaults.go
+++ internal/gss/defaults/defaults.go
@@ -6,7 +6,7 @@ import (
 )
 
 var Div = ast.Styles{
-	Display:         tokens.Display_Block,
+	Display:         ast.Display{tokens.DisplayOutside_Block, tokens.DisplayInside_Flow},
 	Width:           tokens.Width_Auto,
 	Height:          tokens.Height_Auto,
 	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
@@ -16,7 +16,7 @@ var Div = ast.Styles{
 }
 
 var Img = ast.Styles{
-	Display:         tokens.Display_Inline,
+	Display:         ast.Display{tokens.DisplayOutside_Inline, tokens.DisplayInside_Flow},
 	Width:           tokens.Width_Auto,
 	Height:          tokens.Height_Auto,
 	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
@@ -26,11 +26,23 @@ var Img = ast.Styles{
 }
 
 var Span = ast.Styles{
-	Display:         tokens.Display_Inline,
+	Display:         ast.Display{tokens.DisplayOutside_Inline, tokens.DisplayInside_Flow},
 	Width:           tokens.Width_Auto,
 	Height:          tokens.Height_Auto,
 	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
 	FontSize:        ast.FontSize{1.0, tokens.Unit_Em},
 	Color:           tokens.Color_Inherit,
 	BackgroundColor: tokens.BackgroundColor_Inherit,
+}
+
+func For(e ast.Element) *ast.Styles {
+	switch e.(type) {
+	case *ast.Div:
+		return &Div
+	case *ast.Img:
+		return &Img
+	case *ast.Span:
+		return &Span
+	}
+	return nil
 }
