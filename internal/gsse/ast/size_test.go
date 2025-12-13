3d691444e711679380fbcaf8b76f773190c785d8
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-14 01:17:16 +0300

    int/gsse/ast: changes a test to use new `Size.Compare`

--- internal/gsse/ast/size_test.go
+++ internal/gsse/ast/size_test.go
@@ -10,15 +10,13 @@ func TestSize_Positive(t *testing.T) {
 	px := Units(tokens.Unit_Px)
 	a := Size{1, px}
 	b := Size{2, px}
-	c, err := a.Add(b)
+	expected := Size{3, px}
+	got, err := a.Add(b)
 	if err != nil {
 		t.Errorf("act: %v", err)
 	}
-	if c.Number != 3 {
-		t.Errorf("assert, expected 3 for value and got %.0f", c.Number)
-	}
-	if !c.Unit.Compare(px) {
-		t.Errorf("assert, expected %q got %q", px, c.Unit)
+	if !expected.Compare(got) {
+		t.Errorf("assert, expected %q got %q", expected, got)
 	}
 }
 
