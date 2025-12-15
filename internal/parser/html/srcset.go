deb7960c7ed7166ead5654875f03de07cebbe0b5
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-15 17:51:37 +0300

    int/parser/html: organizes package

---
+++ internal/parser/html/srcset.go
@@ -0,0 +1,23 @@
+package html
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func parseSrcSet(s string) (map[float64]string, error) {
+	set := map[float64]string{}
+	for s := range strings.SplitSeq(s, ",") {
+		s = strings.TrimSpace(s)
+		ps := strings.Split(s, " ")
+		if len(ps) == 2 {
+			d, err := strconv.ParseFloat(ps[1], 64)
+			if err != nil {
+				return nil, fmt.Errorf("parsing density number: %w", err)
+			}
+			set[d] = strings.TrimSpace(ps[0])
+		}
+	}
+	return set, nil
+}
