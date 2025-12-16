93b7d9bf66e7d56e449bf3b396dce028071be874
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-16 17:03:59 +0300

    int/ast/gss: adds many missing properties

--- internal/ast/gss/ast.go
+++ internal/ast/gss/ast.go
@@ -10,14 +10,56 @@ type Display struct {
 	Inside  gss.DisplayInside
 }
 
+type BorderEdge struct {
+	Color     gsse.Expr[gsse.Color]
+	Style     string
+	Thickness gsse.Expr[gsse.Pixels]
+}
+
+type BorderCorner struct {
+	Radius gsse.Expr[gsse.Pixels]
+}
+
+type Border struct {
+	Top, Right, Bottom, Left                   BorderEdge
+	TopLeft, TopRight, BottomRight, BottomLeft BorderCorner
+}
+
+type Margin struct {
+	Top, Right, Bottom, Left gsse.Expr[gsse.Pixels]
+}
+
+type Padding struct {
+	Top, Right, Bottom, Left gsse.Expr[gsse.Pixels]
+}
+
+type Font struct {
+	FontFamily []gss.FontFamily       `gss:"font-family"`
+	FontSize   gsse.Expr[gsse.Pixels] `gss:"font-size"`
+	FontWeight gsse.Expr[gsse.Pixels] `gss:"font-weight"`
+}
+
+type Text struct {
+	Color         gsse.Expr[gss.Color]   `gss:"color"`
+	LineHeight    gsse.Expr[gsse.Pixels] `gss:"line-height"`
+	TextAlignment gss.TextAlignment      `gss:"text-alignment"`
+}
+
+type Dimensions struct {
+	Height gss.Height `gss:"height"`
+	Width  gss.Width  `gss:"width"`
+}
+
+// TODO: handle shorthand syntaxes during parsing
 type Styles struct {
-	Display         Display              `gss:"display"`
-	Width           gss.Width            `gss:"width"`
-	Height          gss.Height           `gss:"height"`
-	FontFamily      []gss.FontFamily     `gss:"font-family"`
-	FontSize        gsse.Expr[gsse.Size] `gss:"font-size"`
-	Color           gss.Color            `gss:"color"`
-	BackgroundColor gss.BackgroundColor  `gss:"background-color"`
+	Dimensions      Dimensions
+	Margin          Margin  `gss:"margin"`
+	Padding         Padding `gss:"padding"`
+	Display         Display `gss:"display"`
+	Text            Text
+	Font            Font
+	Border          Border               `gss:"border"`
+	BackgroundColor gsse.Expr[gss.Color] `gss:"background-color"`
 }
 
 type Rule struct {
