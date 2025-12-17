270e98f921d2d345d4a0e0c4998a745770209c16
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-18 00:52:13 +0300

    int/sets: changes the `Permutations` to stabilize left columns and volatilize the right columns

--- internal/sets/sets.go
+++ internal/sets/sets.go
@@ -29,53 +29,37 @@ func Product(sets ...[]string) iter.Seq[[]string] {
 	}
 }
 
-// Heap's Algorithm (Iterative)
-//
-// Going left to right, swaps the first element that isn't
-// swapped as many times as its index number since one of
-// its righthand items gets swapped.
-// Repeation ends when each spot have as many swaps as its
-// index.
-//
-// Chart for count array:
-//
-//	0 0 0 0 0
-//	0 1 0 0 0 (after 0th and 1st swapped)
-//	0 0 1 0 0 (after 0th and 2nd swapped. 1st set 0 as passed)
-//	0 1 1 0 0 (after 0th and 1st swapped. 2nd is untouched)
-//	0 0 2 0 0 ...
-//	...
-//	...
-//	...
-//	0 1 2 3 4
+// This version of Heap's algorithm chooses to stabilize
+// the items on left. Meaning, items at the right are more
+// volatile.
 func Permutations(set []string) iter.Seq[[]string] {
-	// It is normal the [i] moves in both directions. It gets
-	// resetted after each swap.
-	//
-	// The [c] values count how many times the [i]th element
-	// swapped with one of its lefthand items since the last
-	// time one of its righthand items gets swapped.
+	// Going right to left, swaps the first element that isn't
+	// swapped as many times as its reverse index number since
+	// one of its lefthand items swapped.
+	// Repeation ends when each spot have as many swaps as its
+	// index.
 	return func(yield func([]string) bool) {
 		p := slices.Clone(set)
 		if !yield(p) {
 			return
 		}
 		c := make([]int, len(set))
-		for i := 0; i < len(set); {
-			if c[i] < i {
-				if i%2 == 0 {
-					p[0], p[i] = p[i], p[0]
+		last := len(set) - 1
+		for d := last; d >= 0; {
+			if c[d] < last-d {
+				if (last-d)%2 == 0 {
+					p[last], p[d] = p[d], p[last]
 				} else {
-					p[c[i]], p[i] = p[i], p[c[i]]
+					p[last-c[d]], p[d] = p[d], p[last-c[d]]
 				}
 				if !yield(p) {
 					return
 				}
-				c[i]++
-				i = 0
+				c[d]++
+				d = last
 			} else {
-				c[i] = 0
-				i++
+				c[d] = 0
+				d--
 			}
 		}
 	}
--- internal/sets/sets_test.go
+++ internal/sets/sets_test.go
@@ -6,22 +6,53 @@ import (
 	"testing"
 )
 
-func ExamplePermutations() {
+func ExamplePermutations_l3() {
 	for p := range Permutations([]string{"a", "b", "c"}) {
 		fmt.Println(p)
 	}
 	// Output:
 	// [a b c]
-	// [b a c]
-	// [c a b]
 	// [a c b]
 	// [b c a]
+	// [b a c]
+	// [c a b]
 	// [c b a]
+}
+
+func ExamplePermutations_l4() {
+	for p := range Permutations([]string{"a", "b", "c", "d"}) {
+		fmt.Println(p)
+	}
+	// Output:
+	// [a b c d]
+	// [a b d c]
+	// [a c d b]
+	// [a c b d]
+	// [a d b c]
+	// [a d c b]
+	// [b d c a]
+	// [b d a c]
+	// [b c a d]
+	// [b c d a]
+	// [b a d c]
+	// [b a c d]
+	// [c a b d]
+	// [c a d b]
+	// [c b d a]
+	// [c b a d]
+	// [c d a b]
+	// [c d b a]
+	// [d c b a]
+	// [d c a b]
+	// [d b a c]
+	// [d b c a]
+	// [d a c b]
+	// [d a b c]
 }
 
 func TestPermutations_Uniqueness(t *testing.T) {
 	ps := map[string]int{}
-	for p := range Permutations([]string{"a", "b", "c", "d"}) {
+	for p := range Permutations([]string{"a", "b", "c", "d", "e", "h"}) {
 		k := strings.Join(p, "")
 		if _, ok := ps[k]; !ok {
 			ps[k] = 0
