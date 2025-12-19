12e939551c4098c2158b4e80a996bda974e03f39
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-19 14:02:40 +0300

    int/dom: renames to `TextNode`

--- internal/dom/child.go
+++ internal/dom/child.go
@@ -9,9 +9,9 @@ type Child interface {
 func (d Div) GetParent() Parent  { return d.Parent }
 func (i Img) GetParent() Parent  { return i.Parent }
 func (s Span) GetParent() Parent { return s.Parent }
-func (t Text) GetParent() Parent { return t.Parent }
+func (t TextNode) GetParent() Parent { return t.Parent }
 
 func (d *Div) SetParent(p Parent)  { d.Parent = p }
 func (i *Img) SetParent(p Parent)  { i.Parent = p }
 func (s *Span) SetParent(p Parent) { s.Parent = p }
-func (t *Text) SetParent(p Parent) { t.Parent = p }
+func (t *TextNode) SetParent(p Parent) { t.Parent = p }
