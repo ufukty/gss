c0c68837c736a25f0a4b22b21d4b493f76611747
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-19 20:04:15 +0300

    int/dom/defaults: replaces typed values with textual values

--- internal/dom/defaults/defaults.go
+++ internal/dom/defaults/defaults.go
@@ -2,60 +2,116 @@ package defaults
 
 import (
 	"go.ufukty.com/gss/internal/ast/gss"
-	"go.ufukty.com/gss/internal/ast/gsse"
+
 	"go.ufukty.com/gss/internal/ast/html"
-	tokens "go.ufukty.com/gss/internal/tokens/gss"
 )
 
-var Div = gss.Styles{
-	Display:         gss.Display{tokens.DisplayOutside_Block, tokens.DisplayInside_Flow},
-	Width:           tokens.Width_Auto,
-	Height:          tokens.Height_Auto,
-	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
-	FontSize:        gsse.Size{1.0, gsse.Units(tokens.Unit_Em)},
-	Color:           tokens.Color_Inherit,
-	BackgroundColor: tokens.BackgroundColor_Inherit,
-}
+var (
+	noBorder = gss.Borders{
+		Top:    gss.Border{"#000", "none", "0"},
+		Right:  gss.Border{"#000", "none", "0"},
+		Bottom: gss.Border{"#000", "none", "0"},
+		Left:   gss.Border{"#000", "none", "0"},
+	}
+	noBorderRadius = gss.BorderRadiuses{
+		TopLeft:     "0",
+		TopRight:    "0",
+		BottomRight: "0",
+		BottomLeft:  "0",
+	}
+	autoDimensions = gss.Dimensions{
+		Width:  "auto",
+		Height: "auto",
+	}
+	inheritFont = gss.Font{
+		Family: "inherit",
+		Size:   "1em",
+		Weight: "400",
+	}
+	noMargin = gss.Margin{
+		Top:    "0",
+		Right:  "0",
+		Bottom: "0",
+		Left:   "0",
+	}
+	somePadding = gss.Padding{
+		Top:    "10px",
+		Right:  "10px",
+		Bottom: "10px",
+		Left:   "10px",
+	}
+	block = gss.Display{
+		Outside: "block",
+		Inside:  "flow",
+	}
+	inline = gss.Display{
+		Outside: "inline",
+		Inside:  "flow",
+	}
+	inheritText = gss.Text{
+		Color:         "inherit",
+		LineHeight:    "inherit",
+		TextAlignment: "inherit",
+	}
+)
 
-var Html = gss.Styles{
-	Display:         gss.Display{tokens.DisplayOutside_Block, tokens.DisplayInside_Flow},
-	Width:           tokens.Width_Auto,
-	Height:          tokens.Height_Auto,
-	FontFamily:      []tokens.FontFamily{tokens.FontFamily_SansSerif},
-	FontSize:        gsse.Size{12.0, gsse.Units(tokens.Unit_Px)},
-	Color:           "#000",
-	BackgroundColor: "#fff",
-}
-
-var Img = gss.Styles{
-	Display:         gss.Display{tokens.DisplayOutside_Inline, tokens.DisplayInside_Flow},
-	Width:           tokens.Width_Auto,
-	Height:          tokens.Height_Auto,
-	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
-	FontSize:        gsse.Size{1.0, gsse.Units(tokens.Unit_Em)},
-	Color:           tokens.Color_Inherit,
-	BackgroundColor: tokens.BackgroundColor_Inherit,
-}
-
-var Span = gss.Styles{
-	Display:         gss.Display{tokens.DisplayOutside_Inline, tokens.DisplayInside_Flow},
-	Width:           tokens.Width_Auto,
-	Height:          tokens.Height_Auto,
-	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
-	FontSize:        gsse.Size{1.0, gsse.Units(tokens.Unit_Em)},
-	Color:           tokens.Color_Inherit,
-	BackgroundColor: tokens.BackgroundColor_Inherit,
-}
-
-var Text = gss.Styles{
-	Display:         gss.Display{tokens.DisplayOutside_Inline, tokens.DisplayInside_Flow},
-	Width:           tokens.Width_Auto,
-	Height:          tokens.Height_Auto,
-	FontFamily:      []tokens.FontFamily{tokens.FontFamily_Inherit},
-	FontSize:        gsse.Size{1.0, gsse.Units(tokens.Unit_Em)},
-	Color:           tokens.Color_Inherit,
-	BackgroundColor: tokens.BackgroundColor_Inherit,
-}
+var (
+	Div = gss.Styles{
+		BackgroundColor: "none",
+		Border:          noBorder,
+		BorderRadiuses:  noBorderRadius,
+		Dimensions:      autoDimensions,
+		Display:         block,
+		Font:            inheritFont,
+		Margin:          noMargin,
+		Padding:         somePadding,
+		Text:            inheritText,
+	}
+	Html = gss.Styles{
+		BackgroundColor: "#fff",
+		Border:          noBorder,
+		BorderRadiuses:  noBorderRadius,
+		Dimensions:      autoDimensions,
+		Display:         block,
+		Font:            gss.Font{Family: "sans-serif", Size: "14px", Weight: "400"},
+		Margin:          noMargin,
+		Padding:         somePadding,
+		Text:            gss.Text{Color: "#000", LineHeight: "inherit", TextAlignment: "left"},
+	}
+	Img = gss.Styles{
+		BackgroundColor: "none",
+		Border:          noBorder,
+		BorderRadiuses:  noBorderRadius,
+		Dimensions:      autoDimensions,
+		Display:         inline,
+		Font:            inheritFont,
+		Margin:          noMargin,
+		Padding:         somePadding,
+		Text:            inheritText,
+	}
+	Span = gss.Styles{
+		BackgroundColor: "none",
+		Border:          noBorder,
+		BorderRadiuses:  noBorderRadius,
+		Dimensions:      autoDimensions,
+		Display:         inline,
+		Font:            inheritFont,
+		Margin:          noMargin,
+		Padding:         somePadding,
+		Text:            inheritText,
+	}
+	TextNode = gss.Styles{
+		BackgroundColor: "none",
+		Border:          noBorder,
+		BorderRadiuses:  noBorderRadius,
+		Dimensions:      autoDimensions,
+		Display:         inline,
+		Font:            inheritFont,
+		Margin:          noMargin,
+		Padding:         somePadding,
+		Text:            inheritText,
+	}
+)
 
 func For(e html.Element) *gss.Styles {
 	switch e.(type) {
@@ -68,7 +124,7 @@ func For(e html.Element) *gss.Styles {
 	case *html.Span:
 		return &Span
 	case *html.TextNode:
-		return &Text
+		return &TextNode
 	}
 	return nil
 }
