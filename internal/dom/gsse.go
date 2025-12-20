9e120dbf9b5f2abeb7b837c051d8f667c0142202
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-20 20:20:12 +0300

    int/dom: changes resolve methods of expression nodes to work either of other expression nodes and direct `float64` values or in types derived from it. removes redundant code. restricts the use of `Dimension`.  allows mixing units that resolve to same value type like px, pt, em resolving to pixels.

--- internal/dom/gsse.go
+++ internal/dom/gsse.go
@@ -53,31 +53,6 @@ func (d Dimension) String() string {
 	return fmt.Sprintf("%.0f%s", d.Number, d.Unit.String())
 }
 
-func (d Dimension) Add(b Dimension) (Dimension, error) {
-	if !d.Unit.Compare(b.Unit) {
-		return Dimension{}, fmt.Errorf("%s + %s: %w", d, b, ErrIncompatibleUnits)
-	}
-	return Dimension{d.Number + b.Number, d.Unit}, nil
-}
-
-func (d Dimension) Sub(b Dimension) (Dimension, error) {
-	if !d.Unit.Compare(b.Unit) {
-		return Dimension{}, fmt.Errorf("%s - %s: %w", d, b, ErrIncompatibleUnits)
-	}
-	return Dimension{d.Number - b.Number, d.Unit}, nil
-}
-
-func (d Dimension) Mul(b Dimension) (Dimension, error) {
-	return Dimension{d.Number * b.Number, d.Unit.Multiply(b.Unit)}, nil
-}
-
-func (d Dimension) Div(b Dimension) (Dimension, error) {
-	if b.Number == 0 {
-		return Dimension{}, fmt.Errorf("%s / %s: %w", d, b, ErrDivisionByZero)
-	}
-	return Dimension{d.Number / b.Number, d.Unit.Divide(b.Unit)}, nil
-}
-
 func (d Dimension) Resolve(ctx Context, e Element) (Dimension, error) {
 	return d, nil
 }
@@ -104,83 +79,98 @@ type (
 	Ident[Resolving any] struct {
 		Name string
 	}
-	Addition struct {
-		Lhs, Rhs Expr[Dimension]
+	Addition[Resolving ~float64] struct {
+		Lhs, Rhs any
 	}
-	Subtraction struct {
-		Lhs, Rhs Expr[Dimension]
+	Subtraction[Resolving ~float64] struct {
+		Lhs, Rhs any
 	}
-	Multiplication struct {
-		Lhs, Rhs Expr[Dimension]
+	Multiplication[Resolving ~float64] struct {
+		Lhs, Rhs any
 	}
-	Division struct {
-		Dividend, Divisor Expr[Dimension]
+	Division[Resolving ~float64] struct {
+		Dividend, Divisor any
 	}
 )
 
 var (
-	_ Expr[Dimension] = (*Addition)(nil)
-	_ Expr[Dimension] = (*Subtraction)(nil)
-	_ Expr[Dimension] = (*Multiplication)(nil)
-	_ Expr[Dimension] = (*Division)(nil)
+	_ Expr[Pixels] = (*Addition[Pixels])(nil)
+	_ Expr[Pixels] = (*Subtraction[Pixels])(nil)
+	_ Expr[Pixels] = (*Multiplication[Pixels])(nil)
+	_ Expr[Pixels] = (*Division[Pixels])(nil)
 )
+
+func subtree[T any](a any, ctx Context, e Element) (T, error) {
+	switch a := a.(type) {
+	case Expr[T]:
+		return a.Resolve(ctx, e)
+	case T:
+		return a, nil
+	default:
+		var t T
+		return t, fmt.Errorf("value of unknown type: %T", a)
+	}
+}
 
 func (c LightDark) Resolve(ctx Context, e Element) (Color, error) {
 	if ctx.Media.PrefersColorScheme == "dark" {
-		return c.Dark.Resolve(ctx, e)
+		return subtree[Color](c.Dark, ctx, e)
 	}
-	return c.Light.Resolve(ctx, e)
+	return subtree[Color](c.Light, ctx, e)
 }
 
 // FIXME: Fetch identity value from DOM not AST once it is available
 func (i Ident[Resolving]) Resolve(ctx Context, e Element) (Resolving, error)
 
-func (a Addition) Resolve(ctx Context, e Element) (Dimension, error) {
-	l, err := a.Lhs.Resolve(ctx, e)
+func (a Addition[F]) Resolve(ctx Context, e Element) (F, error) {
+	l, err := subtree[F](a.Lhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("lhs: %w", err)
+		return l, fmt.Errorf("left operand: %w", err)
 	}
-	r, err := a.Rhs.Resolve(ctx, e)
+	r, err := subtree[F](a.Rhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("rhs: %w", err)
+		return r, fmt.Errorf("right operand: %w", err)
 	}
-	return l.Add(r)
+	return l + r, nil
 }
 
-func (a Subtraction) Resolve(ctx Context, e Element) (Dimension, error) {
-	l, err := a.Lhs.Resolve(ctx, e)
+func (a Subtraction[F]) Resolve(ctx Context, e Element) (F, error) {
+	l, err := subtree[F](a.Lhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("lhs: %w", err)
+		return l, fmt.Errorf("left operand: %w", err)
 	}
-	r, err := a.Rhs.Resolve(ctx, e)
+	r, err := subtree[F](a.Rhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("rhs: %w", err)
+		return r, fmt.Errorf("right operand: %w", err)
 	}
-	return l.Sub(r)
+	return l - r, nil
 }
 
-func (a Multiplication) Resolve(ctx Context, e Element) (Dimension, error) {
-	l, err := a.Lhs.Resolve(ctx, e)
+func (a Multiplication[F]) Resolve(ctx Context, e Element) (F, error) {
+	l, err := subtree[F](a.Lhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("lhs: %w", err)
+		return l, fmt.Errorf("left operand: %w", err)
 	}
-	r, err := a.Rhs.Resolve(ctx, e)
+	r, err := subtree[F](a.Rhs, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("rhs: %w", err)
+		return r, fmt.Errorf("right operand: %w", err)
 	}
-	return l.Mul(r)
+	return l * r, nil
 }
 
-func (a Division) Resolve(ctx Context, e Element) (Dimension, error) {
-	l, err := a.Dividend.Resolve(ctx, e)
+func (a Division[F]) Resolve(ctx Context, e Element) (F, error) {
+	l, err := subtree[F](a.Dividend, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("lhs: %w", err)
+		return l, fmt.Errorf("left operand: %w", err)
 	}
-	r, err := a.Divisor.Resolve(ctx, e)
+	r, err := subtree[F](a.Divisor, ctx, e)
 	if err != nil {
-		return Dimension{}, fmt.Errorf("rhs: %w", err)
+		return r, fmt.Errorf("right operand: %w", err)
 	}
-	return l.Div(r)
+	if r == 0 {
+		return r, ErrDivisionByZero
+	}
+	return l / r, nil
 }
 
 // Nodes
