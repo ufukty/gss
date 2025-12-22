d95320362fe0d2aa09cecdcda43aa104ae285356
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-22 09:57:32 +0300

    int/dom/units: adds missing css units

---
+++ internal/dom/units/u.go
@@ -0,0 +1,50 @@
+package units
+
+type U string
+
+const (
+	Percent U = "percent"
+)
+
+// Length
+const (
+	Cm  U = "cm"
+	Em  U = "em"
+	In  U = "in"
+	Mm  U = "mm"
+	Pc  U = "pc"
+	Pt  U = "pt"
+	Px  U = "px"
+	Q   U = "q"
+	Rem U = "rem"
+	Vh  U = "vh"
+)
+
+// Angle
+const (
+	Deg  U = "deg"
+	Grad U = "grad"
+	Rad  U = "rad"
+	Turn U = "turn"
+)
+
+// Duration
+const (
+	Ms U = "ms"
+	S  U = "s"
+)
+
+const (
+	Hz  U = "hz"
+	KHz U = "khz"
+)
+
+const (
+	Dpcm U = "dpcm"
+	Dpi  U = "dpi"
+	Dppx U = "dppx"
+)
+
+const (
+	Fr U = "fr"
+)
