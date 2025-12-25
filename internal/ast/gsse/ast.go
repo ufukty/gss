1876727309d55ee5a932f9926d15f9805fba2286
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-25 20:35:53 +0300

    int/ast/gsse: adds the nodes `Calc`, `Min`, `Max`

--- internal/ast/gsse/ast.go
+++ internal/ast/gsse/ast.go
@@ -4,6 +4,9 @@ package gsse
 type (
 	LightDark      struct{ Light, Dark any }
 	Ident          struct{ Name any }
+	Calc           struct{ Expr any }
+	Min            struct{ Exprs []any }
+	Max            struct{ Exprs []any }
 	Addition       struct{ Left, Right any }
 	Subtraction    struct{ Left, Right any }
 	Multiplication struct{ Left, Right any }
