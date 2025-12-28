c0656800a41235a7b76a9c30202cfca8aebd2a23
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-28 23:20:51 +0300

    int/parser/gss: adds function for testing positional shorthanded values for `border` property

--- internal/parser/gss/border_test.go
+++ internal/parser/gss/border_test.go
@@ -22,7 +22,7 @@ func tokenize(in string) ([]css.Token, error) {
 	return p.Values(), nil
 }
 
-func TestParseBorder(t *testing.T) {
+func TestParseBorder_combinations(t *testing.T) {
 	var (
 		colors = []string{"", "red", "#f00", "#F00", "#f000", "#F000", "#ff0000", "#FF0000", "#ff000000", "#FF000000"}
 		styles = []string{"", "solid", "dashed", "dotted"}
@@ -32,6 +32,25 @@ func TestParseBorder(t *testing.T) {
 	for combination := range sets.Product(colors, styles, widths) {
 		input := strings.Join(combination, " ")
 		t.Run(input, func(t *testing.T) {
+			ts, err := tokenize(fmt.Sprintf("border: %s", input))
+			if err != nil {
+				t.Fatalf("prep, unexpected error: %v", err)
+			}
+			ParseBorder(ts)
+		})
+	}
+}
+
+func TestParseBorders_positionalShorthands(t *testing.T) {
+	tcs := map[string]string{
+		"T|LR|B":  "1px solid #000, 2px solid #000, 3px solid #000",
+		"T|R|B|L": "1px solid #000, 2px solid #000, 3px solid #000, 4px solid #000",
+		"TB|LR":   "1px solid #000, 2px solid #000",
+		"TRBL":    "1px solid #000",
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc, func(t *testing.T) {
 			ts, err := tokenize(fmt.Sprintf("border: %s", input))
 			if err != nil {
 				t.Fatalf("prep, unexpected error: %v", err)
