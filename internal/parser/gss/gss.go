cc5302b13ce4050fefb2a52661806627df443edf
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-12-28 23:04:52 +0300

    int/parser/gss: removes unused

--- internal/parser/gss/gss.go
+++ internal/parser/gss/gss.go
@@ -1,23 +1,5 @@
 package gss
 
-import (
-	"io"
-
-	"go.ufukty.com/gss/internal/ast"
-)
-
 func silent[T any](t T, _ error) T {
 	return t
-}
-
-func rule(src io.Reader) (*ast.Rule, error) {
-	return nil, nil
-}
-
-func Gss(r io.Reader) (*ast.Gss, error) {
-	g := &ast.Gss{
-		Rules: []*ast.Rule{},
-	}
-
-	return g, nil
 }
